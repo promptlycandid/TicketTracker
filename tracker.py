56245754988a57d787ca2115a2248a06e979b2ba
PromptlyCandid <salesmangregorysamsa@gmail.com>
2020-12-22 04:15:33 -0600

    Added view open tickets button and function

--- tracker.py
+++ tracker.py
@@ -112,19 +112,19 @@ def newTicket():
 
 ### TODO: View Tickets Function ###
 def viewTickets():
-	cTicket = tk.Tk()
-	cTicket.geometry("1500x1000")
-	cTicket.pack_propagate(False)
-	cTicket.resizable(0, 0)
+	oTicket = tk.Tk()
+	oTicket.geometry("1500x1000")
+	oTicket.pack_propagate(False)
+	oTicket.resizable(0, 0)
 
 	def clear_data():
 		tv1.delete(*tv1.get_children())
 
-	cTicket.title("All Tickets")
+	oTicket.title("All Tickets")
 
 
 	# Frame for Treeview
-	frame1 = tk.LabelFrame(cTicket, text="Ticket Data")
+	frame1 = tk.LabelFrame(oTicket, text="Ticket Data")
 	frame1.place(height=1000, width=1500)
 
 	# Treeview Widget
@@ -152,6 +152,50 @@ def viewTickets():
 		tv1.insert("", "end", values=row)
 	return None
 
+### Open Tickets Function ###
+def openTickets():
+	oTicket = tk.Tk()
+	oTicket.geometry("1500x1000")
+	oTicket.pack_propagate(False)
+	oTicket.resizable(0, 0)
+
+	def clear_data():
+		tv1.delete(*tv1.get_children())
+
+	oTicket.title("Open Tickets")
+
+
+	# Frame for Treeview
+	frame1 = tk.LabelFrame(oTicket, text="Ticket Data")
+	frame1.place(height=1000, width=1500)
+
+	# Treeview Widget
+	tv1 = ttk.Treeview(frame1)
+	tv1.place(relheight=1, relwidth=1)
+
+	treescrolly = tk.Scrollbar(frame1, orient="vertical", command=tv1.yview)
+	treescrollx = tk.Scrollbar(frame1, orient="horizontal", command=tv1.xview)
+	tv1.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set)
+	treescrollx.pack(side="bottom", fill="x")
+	treescrolly.pack(side="right", fill="y")
+
+	# Read file with Pandas
+	file_path = "TicketTracker.ods"
+	excel_filename = r"{}".format(file_path)
+	df = pd.read_excel(excel_filename)
+	open_tickets = df.loc[df['Closed'] != 'X']
+	
+
+	clear_data()
+	tv1["column"] = list(open_tickets.columns)
+	tv1["show"] = "headings"
+	for column in tv1["columns"]:
+		tv1.heading(column, text=column)
+
+	df_rows = open_tickets.to_numpy().tolist()
+	for row in df_rows:
+		tv1.insert("", "end", values=row)
+	return None
 
 
 ### TODO: Close Ticket Function
@@ -171,10 +215,10 @@ def viewTickets():
 # Ticket Tracker main menu buttons
 tk.Button(master, text='New Ticket', command=newTicket).grid(row=6, column=0, pady=4)
 tk.Button(master, text='View Tickets', command=viewTickets).grid(row=6, column=1, pady=4)
-# TODO Button(master, text='Close Ticket', command=closeTicket).grid(row=6, column=1, pady=4)
-# TODO Button(master, text='Edit Ticket', command=editTicket).grid(row=6, column=1, pady=4)
-tk.Button(master, text='Quit', command=master.quit).grid(row=6, column=2, pady=4)
-
+tk.Button(master, text='Open Tickets', command=openTickets).grid(row=6, column=2, pady=4)
+# TODO: tk.Button(master, text='Edit Ticket', command=editTicket).grid(row=6, column=3, pady=4)
+# TODO: tk.Button(master, text='Close Ticket', command=closeTicket).grid(row=6, column=4, pady=4)
+tk.Button(master, text='Quit', command=master.quit).grid(row=6, column=5, pady=4)
 
 
 
